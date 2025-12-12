eb475bd97519e4f0756ec044a514b3ce2c60b742
Naveen <150805268+Naveen-cloud-DevOps@users.noreply.github.com>
2025-12-12 17:37:26 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import pymysql
 from flask_cors import CORS
@@ -116,6 +115,7 @@ def get_users():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     
+
 
 
 
